a55f6fcb811e87f25334c1abf277a13a2098fb7c
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2025-09-30 17:36:15 +0200

    Compiled

---
+++ First_Assignment.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class First_Assignment{
+	
+	public static void main(String[] agrs)
+	{	
+		Scanner input = new Scanner(System.in);
+		String name = input.nextLine();
+		
+		
+		System.out.println(name);
+		System.out.println("I want to be a Backend Java Developer");
+		System.out.println("And my career goal is as a Software Enginner");
+	}
+}
